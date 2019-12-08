d34110772655ab830ad51287c5110e0c9cd64ea8
Daniel Wiese <dan@humon.io>
2019-12-08 14:46:24 -0500

    dont use listings

--- md2pdf.sh
+++ md2pdf.sh
@@ -4,7 +4,7 @@ sed 's/ {linenos=false}&nbsp;//g' "$1" > temp.md; \
 pandoc temp.md \
     --from markdown+yaml_metadata_block \
     --include-in-header ~/.pandoc/headings.tex \
-    --include-in-header ~/.pandoc/listings-code.tex \
+    --include-in-header ~/.pandoc/no-listings-code.tex \
     --include-in-header ~/.pandoc/link-color.tex \
     -V urlcolor=cyan \
     -V linkcolor:blue \
@@ -14,7 +14,6 @@ pandoc temp.md \
     -V monofont="Menlo Regular" \
     -V fontsize=10pt \
     --lua-filter ~/.pandoc/lua-links.lua \
-    --listings \
     --template eisvogel \
     --pdf-engine=xelatex \
     -o "$2"; rm temp.md
