cfd4336d1c517e99da9599d27e828ececa1c637d
Daniel Wiese <dan@humon.io>
2019-12-08 12:35:29 -0500

    basic Pandoc headers and templates

---
+++ md2pdf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sed 's/ {linenos=false}&nbsp;//g' "$1" > temp.md; \
+pandoc temp.md \
+    --from markdown+yaml_metadata_block \
+    --include-in-header ~/.pandoc/headings.tex \
+    --include-in-header ~/.pandoc/inline-code.tex \
+    --include-in-header ~/.pandoc/link-color.tex \
+    -V urlcolor=cyan \
+    -V linkcolor:blue \
+    -V geometry:letter \
+    -V geometry:margin=1in \
+    -V mainfont="SFNS Display" \
+    -V monofont="Menlo Regular" \
+    -V fontsize=10pt \
+    --lua-filter ~/.pandoc/lua-links.lua \
+    --listings \
+    --template eisvogel \
+    --pdf-engine=xelatex \
+    -o "$2"; rm temp.md
