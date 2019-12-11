6eb674f6ec9d68f368ac87731546abfbc55bf599
Daniel Wiese <dan@humon.io>
2019-12-11 11:43:09 -0500

    organize directory

--- md2pdf.sh
+++ md2pdf.sh
@@ -3,9 +3,9 @@
 sed 's/ {linenos=false}&nbsp;//g' "$1" > temp.md; \
 pandoc temp.md \
     --from markdown \
-    --include-in-header ~/.pandoc/headings.tex \
-    --include-in-header ~/.pandoc/no-listings-code.tex \
-    --include-in-header ~/.pandoc/link-color.tex \
+    --include-in-header ~/.pandoc/tex-headers/headings.tex \
+    --include-in-header ~/.pandoc/tex-headers/no-listings-code.tex \
+    --include-in-header ~/.pandoc/tex-headers/link-color.tex \
     -V urlcolor=cyan \
     -V linkcolor:blue \
     -V geometry:letter \
@@ -13,7 +13,7 @@ pandoc temp.md \
     -V mainfont="SFNS Display" \
     -V monofont="Menlo Regular" \
     -V fontsize=10pt \
-    --lua-filter ~/.pandoc/lua-links.lua \
+    --lua-filter ~/.pandoc/filters/lua-links.lua \
     --template eisvogel \
     --pdf-engine=xelatex \
     -o "$2"; rm temp.md
