f450a62e3d0faca3575b50f41cabc8fa67c3ad58
Daniel Wiese <dan@humon.io>
2019-12-12 13:21:51 -0500

    pipe sed into pandoc instead of temporary file

--- md2pdf.sh
+++ md2pdf.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-sed 's/ {linenos=false}&nbsp;//g' "$1" > temp.md; \
-pandoc temp.md \
-    --from markdown \
-    --include-in-header ~/.pandoc/tex-headers/headings.tex \
-    --include-in-header ~/.pandoc/tex-headers/no-listings-code.tex \
-    --include-in-header ~/.pandoc/tex-headers/link-color.tex \
-    -V urlcolor=cyan \
-    -V linkcolor:blue \
-    -V geometry:letter \
-    -V geometry:margin=1in \
-    -V mainfont="SFNS Display" \
-    -V monofont="Menlo Regular" \
-    -V fontsize=10pt \
-    --lua-filter ~/.pandoc/filters/lua-links.lua \
-    --template eisvogel \
-    --pdf-engine=xelatex \
-    -o "$2"; rm temp.md
+sed 's/ {linenos=false}&nbsp;//g' "$1" | pandoc \
+  --from markdown \
+  --include-in-header ~/.pandoc/tex-headers/headings.tex \
+  --include-in-header ~/.pandoc/tex-headers/no-listings-code.tex \
+  --include-in-header ~/.pandoc/tex-headers/link-color.tex \
+  -V urlcolor=cyan \
+  -V linkcolor:blue \
+  -V geometry:letter \
+  -V geometry:margin=1in \
+  -V mainfont="SFNS Display" \
+  -V monofont="Menlo Regular" \
+  -V fontsize=10pt \
+  --lua-filter ~/.pandoc/filters/lua-links.lua \
+  --template eisvogel \
+  --pdf-engine=xelatex \
+  -o "$2"
