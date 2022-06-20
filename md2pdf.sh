23684df6400fff6bf7418bb5d5b6f9fefaa55ac4
Daniel Wiese <dpwiese@gmail.com>
2022-06-20 10:47:50 -0400

    add latex header to center figure captions
    still only working for figures in markdown, not html

--- md2pdf.sh
+++ md2pdf.sh
@@ -5,6 +5,7 @@ sed 's/ {linenos=false}&nbsp;//g' "$1" | pandoc \
   --include-in-header ~/.pandoc/tex-headers/headings.tex \
   --include-in-header ~/.pandoc/tex-headers/no-listings-code.tex \
   --include-in-header ~/.pandoc/tex-headers/link-color.tex \
+  --include-in-header ~/.pandoc/tex-headers/center-captions.tex \
   -V urlcolor=cyan \
   -V linkcolor:blue \
   -V geometry:margin=1in \
