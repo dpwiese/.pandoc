7d364bc153abfa0e9b5c65d39e87517a2bca2dea
Daniel Wiese <dpwiese@gmail.com>
2024-03-17 20:08:15 -0400

    set-secnum-depth.lua whitespace only

--- filters/set-secnum-depth.lua
+++ filters/set-secnum-depth.lua
@@ -2,10 +2,10 @@
 See: https://github.com/jgm/pandoc/issues/6459
 
 The behavior achieved by this filter can also be achieved by using
- the below options in the yaml header. However, these options may
- be superceded by Latex headers which, when included, can force
- sections to be numbered, for example. So this filter may come in
- handy, but otherwise try not to use it.
+the below options in the yaml header. However, these options may
+be superceded by Latex headers which, when included, can force
+sections to be numbered, for example. So this filter may come in
+handy, but otherwise try not to use it.
 
 numbersections: true
 secnumdepth: 1
