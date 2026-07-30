3ae9397cafedbde90a18d87caf21e8e0e3d4274c
Daniel Wiese <dpwiese@gmail.com>
2026-07-30 17:21:02 -0500

    when parsing html also raw inline

--- filters/parse-html.lua
+++ filters/parse-html.lua
@@ -5,3 +5,10 @@ function RawBlock (raw)
     and pandoc.read(raw.text, 'html').blocks
     or raw
 end
+
+function RawInline (raw)
+  return raw.format:match 'html'
+    and raw.text:match `^</?br%s*/?>$'
+    and pandoc.LineBreak()
+    or raw
+end
